68efdcbab2508c1bc47155665561f4ccbc35719e
Honbra <54026315+HonbraDev@users.noreply.github.com>
2021-06-21 17:40:19 +0200

    Added extra

--- index.d.ts
+++ index.d.ts
@@ -1,39 +1,45 @@
 export function pingWithPromise(ip: string, port?: number): Promise<ServerInfo>;
-export function ping(ip: string, port: number, callback: (info: ServerInfo) => any): void;
+export function ping(
+  ip: string,
+  port: number,
+  callback: (info: ServerInfo) => any
+): void;
 
 export interface ServerInfo {
-    version: ServerVersion;
-    players: ServerPlayers;
-    description: ServerDescription;
-    favicon: ServerFavicon;
-    modinfo: ServerModInfo;
-    ping: ServerPing;
+  version: ServerVersion;
+  players: ServerPlayers;
+  description: ServerDescription;
+  favicon: ServerFavicon;
+  modinfo: ServerModInfo;
+  ping: ServerPing;
 }
 
 export interface ServerVersion {
-    name: string;
-    protocol: number;
+  name: string;
+  protocol: number;
 }
 
 export interface ServerPlayers {
-    online: number;
-    max: number;
+  online: number;
+  max: number;
 }
 
 export interface ServerDescription {
-    text: string;
+  text: string;
+  // JSON chat component
+  extra: any[];
 }
 
 export type ServerFavicon = string;
 
 export interface ServerModInfo {
-    type: string;
-    modList: ServerMod[];
+  type: string;
+  modList: ServerMod[];
 }
 
 export interface ServerMod {
-    modid: string;
-    version: string;
+  modid: string;
+  version: string;
 }
 
 export type ServerPing = number;
